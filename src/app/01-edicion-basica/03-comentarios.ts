2022a713cb80911f4e894bb73bf2622cea2e36cf
Juan Gerardo Romero Uro <jgromerouro@gmail.com>
2023-04-07 12:42:10 -0300

    sección 2 terminada : Ediciones y tips básicos

--- src/app/01-edicion-basica/03-comentarios.ts
+++ src/app/01-edicion-basica/03-comentarios.ts
@@ -11,13 +11,11 @@ const a = 10;
 const b = 20;
 const c = { a, b };
 
+// const a = 30;
+// const b = 40;
+// const c = { a, b };
 
-const a = 30;
-const b = 40;
-const c = { a, b };
-
-
-console.log( a, b, c );
+console.log(a, b, c);
 
 // Resultado final
 // Sólo dejar un bloque comentado
